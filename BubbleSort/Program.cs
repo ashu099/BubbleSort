e7f9bad762dbb2a27a90ee7a7aad7863e80036cf
Ashutosh <ashutosh.chaturvedi@euromonitor.com>
2024-07-11 22:21:55 +0530

    test

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Test
+{
+    class Test
+    {
+
+        int a, b;
+        public void Swap(int a, int b)
+        {
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine($"{0},{1}", a, b);
+
+        }
+        public static void Main()
+        {
+            Test t = new Test();
+            t.Swap(5, 6);
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Test
+    {
+
+        int k, ptr,temp;
+        int[] arr = { 23, 12, 45, 34, 42, 67, 87, 65, 90, 84 };
+       
+        public void Swap()
+        {
+            Console.WriteLine("before sorting");
+            for (int i = 0; i < arr.Length; i++) //length =10
+            {
+               
+                Console.WriteLine(arr[i]);
+            };
+            for (int i = 0; i < arr.Length-1-1; i++)//length=8
+            {
+                ptr = 0;
+                while (ptr<=(arr.Length-i-1-1))//first length =8, then 7,6,5,4,3,2,1
+                {
+                    if (arr[ptr]>arr[ptr+1])
+                    {
+                        temp = arr[ptr];
+                        arr[ptr] = arr[ptr + 1];
+                        arr[ptr + 1] = temp;
+                    }
+                    ptr++;
+
+                }
+            }
+            //a = a * b;
+            //b = a / b;
+            //a = a / b;
+            Console.WriteLine("after sorting");
+            for (int i = 0; i < arr.Length; i++)
+            {
+              
+                Console.WriteLine(arr[i]);
+            };
+
+        }
+        public static void Main()
+        {
+           
+            Test t = new Test();
+            t.Swap();
+        }
+
+    }
+}
